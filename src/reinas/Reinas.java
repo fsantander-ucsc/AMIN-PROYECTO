4225e6ec7c1f1ce15a212b68669be7f0ade31cfa
Felipe uwu <65966572+fsantander-ucsc@users.noreply.github.com>
2021-10-24 13:45:27 -0300

    Add files via upload
    Se agrega la idea de mutación de los hijos

--- src/reinas/Reinas.java
+++ src/reinas/Reinas.java
@@ -10,6 +10,7 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Random;
+import static reinas.Tablero.dimension;
 
 /**
  *
@@ -22,103 +23,173 @@ public class Reinas {
      */
     public static void main(String[] args) {
 
-        //int seed = Integer.valueOf(args[0]);
-        //int tablero = Integer.valueOf(args[1]);
-        //int tamano_población = Integer.valueOf(args[2]);
-        //int probabilidad_cruza = Integer.valueOf(args[3]);
-        //int probabilidad_mutacion =Integer.valueOf(args[4]);
-        //int numero_iteraciones=Integer.valueOf(args[5]);
-        System.out.println("Inicio");
-        int dimension, seed, iteraciones;
-        double probCruza;
+        //se definen las variables a utilizar
+        int tablero, seed, numero_iteraciones, auxIteracion, tamano_poblacion, mejorFitness, numero_hijo, numero_mejor_hijo;
+        double probabilidad_cruza, probabilidad_mutacion;
+        List<Tablero> padres = new ArrayList<>();
+        List<Tablero> hijos = new ArrayList<>();
+        List<Integer> reinas = null;
+        List<Double> ruleta = new ArrayList<>();
 
-        List<Tablero> lista = new ArrayList<>();
+        //esto sólo se hace para que no me diga que los valores no están asignados
+        //es que la captura está siendo hecha en un try/catch
+        tamano_poblacion = 1000;
+        numero_iteraciones = 100000;
+        tablero = 100;
+        seed = 1;
+        probabilidad_cruza = 5 / 100.0;
+        probabilidad_mutacion = 5 / 100.0;
+        
+        //tp 100, ti 10000 t 20  s2 pc 5 pm 5 da resultado todas las iteraciones con mal fit
+        
+        /*
+        try {
+            if (args.length < 6) {
+                System.err.println("El mínimo de parámetros es 6");
+                System.exit(1);
+            }
+            seed = Integer.valueOf(args[0]);
+            tablero = Integer.valueOf(args[1]);
+            tamano_poblacion = Integer.valueOf(args[2]);
+            probabilidad_cruza = Integer.valueOf(args[3]);
+            probabilidad_mutacion = Integer.valueOf(args[4]);
+            numero_iteraciones = Integer.valueOf(args[5]);
 
-        iteraciones = 1000000;
-        dimension = 5;
-        seed = 4;
-        probCruza = 5.0 / 100.0;
+        } catch (Exception e) {
+            System.err.println(e);
+            System.exit(1);
+        }
+         */
+        
+        auxIteracion = 0;
+        numero_hijo = 0;
+        numero_mejor_hijo = 0;
+        //el mejor fitness se inicializa como max valu
+        //es casi como decir "infinito"
+        mejorFitness = Integer.MAX_VALUE;
+
+        System.out.println("Inicialización");
 
         Random random = new Random();
-
-        //inicialización de la semilla
+        //inicialización de la semilla       
         random.setSeed(seed);
-        Tablero.dimension = dimension;
+        //se guarda la dimensipon del tablero dentro del objeto
+        //así es más sencillo recurrir a el dentro del objeto
+        Tablero.dimension = tablero;
 
-        for (int i = 1; i < iteraciones + 1; i++) {
+        for (int i = 1; i < tamano_poblacion + 1; i++) { 
             int rndNumber = random.nextInt(100) + 1;
-            //se ingresa la seed al tablero
-            Tablero test = new Tablero(rndNumber);
-            //test.printTablero();
-            //System.out.println("");
-            lista.add(test);
+            Tablero test = new Tablero(rndNumber);            
+            padres.add(test);
         }
-       
-        
-        System.out.println("Fin inicialización");
 
-        List<Double> ruleta = new ArrayList<>();
+        System.out.println("Fin inicialización");
 
         //ruleta es la suma acumulada de las proporciones
         //a mayor proporción se tiene un espacio más grande dentro de la ruleta
-        System.out.println("Inicio Ruleta");
-        ruleta.add(lista.get(0).getProporcion());
-        for (int i = 1; i < lista.size(); i++) {            
-            ruleta.add(ruleta.get(i - 1) + lista.get(i).getProporcion());
-        }
-        System.out.println("Fin Ruleta");
+        hacerRuleta(padres, ruleta);
 
         //nota, la posición en la ruleta es la posición dentro de la lista de tableros
         //por lo que el valor de ruleta podría ser un atributo dentro del objeto tablero
-        Random rnd = new Random();
-
+       
         //se inicializan estas variables del objeto
         //para poder calcular nuevos valores de proporción
         Tablero.suma = 0;
         Tablero.sumaFit = 0;
 
-        List<Tablero> hijos = new ArrayList<>();
         System.out.println("Inicio Cruza");
-        for (int i = 0; i < iteraciones; i++) {
+        while (auxIteracion < numero_iteraciones && mejorFitness != 1) {
 
             int t1 = -1, t2 = -1;
-
             //guardamos la posición del objeto dentro de t1 y t2
-            //como es menos -1 y hablamos de una lista, ningún objeto debería
+            //como es -1 y hablamos de una lista, ningún objeto debería
             //llegar a ese valor
             //luego se compara ese valor para saber si los dos padres son el mismo objeto
             //se podría guardar el objeto, pero consume un poco más de memoria
             double rnd_1, rnd_2;
 
             while (t1 == t2) {
-                rnd_1 = rnd.nextDouble();
-                rnd_2 = rnd.nextDouble();
+                rnd_1 = random.nextDouble();
+                rnd_2 = random.nextDouble();
                 t1 = encontrarEnRuleta(ruleta, rnd_1);
                 t2 = encontrarEnRuleta(ruleta, rnd_2);
             }
 
             //numero random a ver si se cruzan
-            double rnd_3 = rnd.nextDouble();
-            if (rnd_3 <= probCruza) {
-                Tablero padre = lista.get(t1);
-                Tablero madre = lista.get(t2);
-                //se crea un hijo a partir de los padres
-                hijos.add(new Tablero(padre, madre));                
+            double rnd_3 = random.nextDouble();
+            
+            if (rnd_3 <= probabilidad_cruza) {
+                //se recuperan los padres en base a la ruleta
+                Tablero padre = padres.get(t1);
+                Tablero madre = padres.get(t2);
+
+                //proporción de los genes padres que pasasrá al hijo
+                int proporcion = (int) (tablero * random.nextDouble());
+
+                //se crean dos hijos a partir de los padres      
+                //se hace un if a ver si se hace o no mutación 
+                int rndNumber =random.nextInt();
+                Tablero h1 = probabilidad_mutacion < random.nextDouble() ? new Tablero(rndNumber, padre, madre, proporcion, true) : new Tablero( rndNumber,padre, madre, proporcion, false);
+                Tablero h2 = probabilidad_mutacion < random.nextDouble() ? new Tablero( rndNumber, madre, padre, proporcion, true) : new Tablero(rndNumber, madre, padre, proporcion, false);
+
+                hijos.add(h1);
+                hijos.add(h2);
+                numero_hijo += 2;
+
+                if (h1.getFitness() < mejorFitness) {
+                    mejorFitness = h1.getFitness();
+                    reinas = new ArrayList<>(h1.getListaReinas());
+                    numero_mejor_hijo = numero_hijo;
+
+                }
+                if (h2.getFitness() < mejorFitness) {
+                    mejorFitness = h2.getFitness();
+                    reinas = new ArrayList<>(h1.getListaReinas());
+                    numero_mejor_hijo = numero_hijo;
+                }
+
+                //En este punto se podría elegir otro método para escoger el
+                //siguiente padre, pero acá los hijos sí o sí se vuelven padres
+                if (hijos.size() >= padres.size()) {
+                    //se aumentan las hiteraciones
+                    auxIteracion++;
+                    //los hijos se vuelven los padres
+                    padres = new ArrayList<>(hijos);
+                    //limpiamos la lista de hijos por seguridadad;
+                    hijos.clear();
+                    //limpiamos la ruleta por seguridad
+                    //y se genera con la población hija
+                    ruleta.clear();
+                    hacerRuleta(padres, ruleta);
+                    //reiniciamos suma y suma fit de la clase Tablero
+                    //se reinician al final pues antes se necesitan para hacer la ruleta
+                    Tablero.suma = 0;
+                    Tablero.sumaFit = 0;
+
+                }
             }
         }
-        int count =0;
-         for(Tablero t : hijos){
-            if(t.getFitness()==1)
-               count++;
-        }
-        System.out.println("Iteraciones: "+ iteraciones);        
-        System.out.println("Cruzas totales: " + hijos.size());
-        System.out.println("Hijos fit 1: "+count);
-        double truncate = Math.floor(count*100.0/hijos.size() * 1000.0) / 1000.0;
-        System.out.println("Correspondiente al "+truncate+"%");
+
+        System.out.println("-----------------------------------");
+        System.out.println("Poblacion: " + tamano_poblacion);
+        System.out.println("Iteraciones: " + numero_iteraciones);
+        System.out.println("Probabilidad de cruza: " + probabilidad_cruza);
+        System.out.println("Probabilidad de mutación: " + probabilidad_mutacion);
+        System.out.println("-----------------------------------");
+        System.out.println("Iteraciones hechas: " + auxIteracion);
+        System.out.println("Hijos totales: " + numero_hijo);
+        System.out.println("Hijo con mejor Fitness: " + numero_mejor_hijo);
+        System.out.println("Mejor fit: " + mejorFitness);
+        System.out.println("Reinas: " + reinas);
+        //imprimeTablero(reinas, tablero);
 
     }
-
+    /**
+     * 
+     * @param lista lista con los valores de ruleta
+     * @param number un número de cualquier tipo
+     * @return Integer con la posición del numero dentro de la lista
+     */
     static private int encontrarEnRuleta(List lista, Number number) {
         //retorna el posición del elemento igual o menor dentro de la lista
         //se asume que la lista está ordenada y contine números de algúntipo
@@ -134,4 +205,33 @@ public class Reinas {
         }
         return aux;
     }
+    /**
+     * 
+     * @param reinas lista con las reinas
+     * @param tablero largo o ancho del tablero
+     */
+    static private void imprimeTablero(List<Integer> reinas, int tablero) {
+        for (int i = 0; i < tablero; i++) {
+            int reina = reinas.get(i);
+            for (int j = 0; j < tablero; j++) {
+                System.out.print((reina == j ? 1 : 0) + " ");
+            }
+            System.out.println(" ");
+        }
+    }
+    
+    /**
+     * 
+     * @param lista lista de la que se obtendran los valores de ruleta
+     * @param ruleta lista en la que se almacenarán lso valores de ruleta
+     */
+    static private void hacerRuleta(List<Tablero> lista, List<Double> ruleta) {
+
+        ruleta.add(lista.get(0).getProporcion());
+        for (int i = 1; i < lista.size(); i++) {
+            ruleta.add(ruleta.get(i - 1) + lista.get(i).getProporcion());
+        }
+
+    }
 }
+
--- src/reinas/Tablero.java
+++ src/reinas/Tablero.java
@@ -17,35 +17,49 @@ import java.util.Random;
  */
 public class Tablero {
 
-    private int tablero[][];
     private int reinas = 0;
-    private int seed = 0;
+    
+    //el valor perfecto de fitnes es 1, cualquier otro valor ya entra en decadencia
     private int fitness = 1;
-    private double fitnessProporcional = 0.0;
-    private double ruleta = 0.0;
+    //el proporcional a mayor valor es mejor
+    private double fitnessProporcional = 1.0;
     private List<Integer> listaReinas = new ArrayList<>();
 
     static int suma = 0;
     static double sumaFit = 0;
-    static int suma_aux = 0;
-    static double sumaFit_aux = 0;
-    static int dimension = 0;
+    //valor por defecto 4, porque es el mínimo que regreesa una solución
+    //así el programa tampoco cae por tener dimensión 0, sería raro
+    static int dimension = 4;
 
     public Tablero(int seed) {
-
-        this.seed = seed;
-        inicializar();
+        inicializar(seed);
         calcularFitness();
     }
 
-    public Tablero(Tablero padre, Tablero madre) {
+    /**
+     *
+     * @param seed semilla del tablero, basicamente para hacer el shuffle
+     * @param padre Tablero Padre
+     * @param madre Tablero Madre
+     * @param proporcion Proporción en la que se dividirán los genes
+     * @param mutacion booleano que indica si ocurre o no una mutación de genes
+     */
+    public Tablero(int seed, Tablero padre, Tablero madre, int proporcion, boolean mutacion) {
 
-        inicializarConCruza(padre, madre);
+        inicializarConCruza(padre, madre, proporcion);
+
+        if (mutacion) {
+            mutacion(seed);
+        }
+
         calcularFitness();
-
     }
 
-    private void inicializar() {
+    /**
+     *
+     * @param seed semilla de la inicialización, se usa para el shuffle
+     */
+    private void inicializar(int seed) {
 
         // se inicializan las reinas en modo de un vector 
         // la posición i representa la columna 
@@ -56,22 +70,20 @@ public class Tablero {
         }
         //se revuelven las reinas para que se posicionen de manera randomica
         //al agregar un segundo parámetro de tipo random, podemos poner una seed en 
-        // al revolver los item y siempre se revolverán igual
-        Collections.shuffle(this.listaReinas, new Random(this.seed));
+        // al revolver los item y siempre se revolverán igual        
+
+        Collections.shuffle(this.listaReinas, new Random(seed));
 
     }
 
-    private void inicializarConCruza(Tablero padre, Tablero madre) {
+    private void inicializarConCruza(Tablero padre, Tablero madre, int proporcion) {
 
         //clonamos la lista de reinas del padre
-        //de esta forma queda no referenciada
-        Random rnd = new Random();
-        //proporción de los padres que pasasrá al hijo
-        int proporcion = (int) (dimension * rnd.nextDouble());
+        //de esta forma queda no referenciada       
         //subPadre corresponde a toda la lista padre
         List<Integer> subPadre = new ArrayList<>(padre.getListaReinas());
         //subMadre va desde el valor proporción hasta el final de la lista
-        List<Integer> subMadre = new ArrayList<>(padre.getListaReinas().subList(proporcion, dimension));
+        List<Integer> subMadre = new ArrayList<>(madre.getListaReinas().subList(proporcion, dimension));
 
         //agregamos TODA la proporción del padre al hijo
         //y removemos el elemento de la lista subPadre
@@ -106,10 +118,36 @@ public class Tablero {
 
     }
 
+    /**
+     *
+     * @param seed semilla de la mutación, de esta forma se generan numero
+     * aleatoreos siempre
+     */
+    private void mutacion(int seed) {
+        //intercambia dos reinas para generar una mutación
+
+        Random rnd = new Random();
+        rnd.setSeed(seed);
+
+        int rnd_1 = 0, rnd_2 = 0;
+
+        while (rnd_1 == rnd_2) {
+            rnd_1 = rnd.nextInt(dimension);
+            rnd_2 = rnd.nextInt(dimension);
+        }
+
+        Collections.swap(this.listaReinas, rnd_1, rnd_2);
+    }
+
     private void calcularFitness() {
+
+        //toma la lista de reinas del objeto y calcula el fitness del tablero generado
+        //en this.fitness se guarda el fitness del tablero
+        //a mayor valor, peor es el fitness
         List<Integer> colDiagonal_positiva = new ArrayList<>();
         List<Integer> colDiagonal_negativa = new ArrayList<>();
 
+        //se revisan ambas diagonales en un mismo ciclo
         for (int i = 0; i < dimension; i++) {
 
             int fila = i;
@@ -119,12 +157,10 @@ public class Tablero {
             int aux = (fila - columna);
             if (colDiagonal_positiva.contains(aux)) {
                 this.fitness++;
-                //System.out.println("x: "+fila+" y: "+columna);
             } else {
                 colDiagonal_positiva.add(aux);
-                //System.out.println("x: "+fila+" y: "+columna);
             }
-            //Colisionesn en diagonal invesa
+            //Colisiones en diagonal invesa
             int aux2 = fila + columna;
             if (colDiagonal_negativa.contains(aux2)) {
                 this.fitness++;
@@ -133,22 +169,25 @@ public class Tablero {
             }
         }
 
+        //de manera estática se almacena la suma de todos los fitnes
         this.suma += this.fitness;
+        //para el objeto también se almacena el fitness proporcional
         this.fitnessProporcional = 1.0 / (double) this.fitness;
+        //sumaFit es lo mismo que suma, pero con los valores proporcionales
+        //se pueden usar ambos en diferentes escenarios
         this.sumaFit += this.fitnessProporcional;
 
     }
 
     public void printTablero() {
-        this.tablero = new int[dimension][dimension];
 
+        //imprime el tablero y ya, no hay mucho que hacer
         //se ponen las reinas en el tablero 
         //en realidad no es necesario, pero sirve para hacer el print
         for (int i = 0; i < dimension; i++) {
             int reina = this.listaReinas.get(i);
             for (int j = 0; j < dimension; j++) {
-                this.tablero[i][j] = (reina == j) ? 1 : 0;
-                System.out.print(this.tablero[i][j] + " ");
+                System.out.print((reina == j ? 1 : 0) + " ");
             }
             System.out.println(" ");
         }
@@ -159,8 +198,8 @@ public class Tablero {
         System.out.println("Fitness: " + this.fitness);
     }
 
-    public double getFitness() {
-        return (double) this.fitness;
+    public int getFitness() {
+        return (int) this.fitness;
     }
 
     public List<Integer> getListaReinas() {
@@ -170,9 +209,4 @@ public class Tablero {
     public double getProporcion() {
         return this.fitnessProporcional / this.sumaFit;
     }
-
-    public void setRuleta(double numero) {
-        this.ruleta = numero;
-    }
-
 }
